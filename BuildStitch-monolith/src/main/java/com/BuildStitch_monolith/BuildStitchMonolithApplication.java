d3f9b152c5fbf4ae2bf5e7325bf33a2a2283c937
Akhil Chandra <gvakhilca1@gmail.com>
2026-08-11 02:43:39 +0530

    Initial commit

---
+++ BuildStitch-monolith/src/main/java/com/BuildStitch_monolith/BuildStitchMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.BuildStitch_monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildStitchMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildStitchMonolithApplication.class, args);
+	}
+
+}
---
+++ BuildStitch-monolith/src/test/java/com/BuildStitch_monolith/BuildStitchMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BuildStitch_monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildStitchMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
